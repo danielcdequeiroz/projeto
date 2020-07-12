7a7eb9cb80ea1b4b1ac7eba1ac3fd339a86a5fd7
danielcdequeiroz <68139148+danielcdequeiroz@users.noreply.github.com>
2020-07-12 14:32:19 -0300

    Update alterar.php

--- alterar.php
+++ alterar.php
@@ -21,11 +21,11 @@ $row_usuario = mysqli_fetch_assoc($resultado_usuario);
 <body>
 
  <nav class="navbar navbar-expand-md navbar navbar-dark fixed-top">
-   <a class="navbar-brand" href="#">Projeto</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
+    <a class="navbar-brand" href="#">Projeto</a>
+ <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
     <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
+    </button>
+ <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">     
       <li class="nav-item">
         <a class="nav-link" href="cadastro.php">Cadastrar</a>
@@ -38,8 +38,8 @@ $row_usuario = mysqli_fetch_assoc($resultado_usuario);
       </li>      
     </ul>
   </div>      
-    </div>
-  </nav>
+ </div>
+</nav>
 
 <div class="container">
     <div class="d-flex justify-content-center h-100">
@@ -50,19 +50,19 @@ $row_usuario = mysqli_fetch_assoc($resultado_usuario);
             <div class="card-body">
                 <form method="POST" action="atualiza.php">                    
                 <input type="hidden" name="codcontato" value="<?php echo $row_usuario['codcontato']; ?>">
-                     <label class="control-label">Nome</label><br>
-                     <div class="input-group form-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fas fa-key"></i></span>
-                        </div>
-                     <input type="text" class="form-control" name="nome" value="<?php echo $row_usuario['nome']; ?>" required="required"><br><br>
-                        </div>
-             <label class="control-label">Sexo</label><br>
-             <div class="input-group form-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fas fa-key"></i></span>
-                        </div>
-             <select class="form-control" name="sexo" required="required">
+                <label class="control-label">Nome</label><br>
+                    <div class="input-group form-group">
+                       <div class="input-group-prepend">
+                           <span class="input-group-text"><i class="fas fa-key"></i></span>
+                       </div>
+                <input type="text" class="form-control" name="nome" value="<?php echo $row_usuario['nome']; ?>" required="required"><br><br>
+                       </div>
+                <label class="control-label">Sexo</label><br>
+                <div class="input-group form-group">
+                      <div class="input-group-prepend">
+                           <span class="input-group-text"><i class="fas fa-key"></i></span>
+                      </div>
+                 <select class="form-control" name="sexo" required="required">
                     <?php
                         $result_niveis_acessos = "SELECT * FROM cadastro WHERE codcontato = '$codcontato'";
                         $resultado_niveis_acesso = mysqli_query($con, $result_niveis_acessos);
@@ -70,67 +70,61 @@ $row_usuario = mysqli_fetch_assoc($resultado_usuario);
                             <option><?php echo $row_niveis_acessos['sexo']; ?></option> <?php
                         }
                     ?>
-                     <option value="Masculino">Masculino</option>
-                     <option value="Feminino">Feminino</option>
-                </select><br><br>
+                  <option value="Masculino">Masculino</option>
+                  <option value="Feminino">Feminino</option>
+                  </select><br><br>
             </div>
-              <label class="control-label">Cidade</label><br>
-              <div class="input-group form-group">
+                 <label class="control-label">Cidade</label><br>
+                 <div class="input-group form-group">
                         <div class="input-group-prepend">
                             <span class="input-group-text"><i class="fas fa-key"></i></span>
                         </div>
-              <input type="text" class="form-control" name="cidade" value="<?php echo $row_usuario['cidade']; ?>" required="required"><br><br>
-              </div>
-
-               <label class="control-label">Data</label><br>
-
-               <div class="input-group form-group">
+                 <input type="text" class="form-control" name="cidade" value="<?php echo $row_usuario['cidade']; ?>" required="required"><br><br>
+                 </div>
+                 <label class="control-label">Data</label><br>
+                 <div class="input-group form-group">
                         <div class="input-group-prepend">
                             <span class="input-group-text"><i class="fas fa-key"></i></span>
                         </div>
-               <input type="date" class="form-control" name="data" value="<?php echo $row_usuario['data']; ?>" required="required"><br><br>
-               </div>
-               <div class="form-group">
-                        <input type="submit" value="Alterar" class="btn btn-info login_btn">
-                    </div>
-                <div class="card-footer">
-                  <div class="d-flex justify-content-center links">
+                 <input type="date" class="form-control" name="data" value="<?php echo $row_usuario['data']; ?>" required="required"><br><br>
+                 </div>
+                 <div class="form-group">
+                 <input type="submit" value="Alterar" class="btn btn-info login_btn">
+                 </div>
+                 <div class="card-footer">
+                 <div class="d-flex justify-content-center links">
                     Voltar para lista ? <a href="editar.php">Editar</a>
-                </div>
-                   
+                 </div>                   
                     </div>
-                </form>
-            </div>
-                
-            </div>
+                 </form>
+            </div>                
+          </div>
         </div>
-    </div>
+      </div>
 </div>
 
 <footer>
-            <div class="text-white bg-dark" draggable="true" >
-                <div class="container">
-                  <div class="row">
+      <div class="text-white bg-dark" draggable="true" >
+           <div class="container">
+                <div class="row">
                     <div class="p-5 col-md-4">
-                        <h4 class="mb-4 marge">Projeto desenvolvido por</h4>  
-                        <p class="text-center">Nome: Daniel Cunha de Queiroz</p>            
+                       <h4 class="mb-4 marge">Projeto desenvolvido por</h4>  
+                       <p class="text-center">Nome: Daniel Cunha de Queiroz</p>            
                      </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-md-12 mt-3">
-                      <p class="text-center">© Copyright 2020 Projeto - Todos os Direitos Reservados. </p>
-                    </div>
+                 </div>
+             <div class="row">
+                  <div class="col-md-12 mt-3">
+                     <p class="text-center">© Copyright 2020 Projeto - Todos os Direitos Reservados. </p>
                    </div>
-                  </div>
+                </div>
               </div>
-             
-        </footer>
+          </div>             
+</footer>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="js/bootstrap.min.js"></script>
-    
-    
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="js/bootstrap.min.js"></script>
+
 </body>
 </html>
 
