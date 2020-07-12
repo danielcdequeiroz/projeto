c95c07a8077f68c4ca1a1d3714756338f785d9c0
Daniel Cunha de Queiroz <danielcdequeiroz@hotmail.com>
2020-07-11 21:34:29 -0300

    Projeto

--- cadastro.php
+++ cadastro.php
@@ -1,58 +1,29 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html>
 <head>
-	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Projeto</title>
-    <link rel="stylesheet" type="text/css" href="css/estilo3.css">
-    <link rel="shortcut icon" href="css/img/icone.ico" type="image/x-icon" />
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
+	<title>Projeto</title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+	<!--Custom styles-->
+	<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-  <nav class="navbar navbar-expand-md navbar navbar-dark fixed-top"style="background-color: black;">
-   <a class="navbar-brand" href="#">Projeto</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">     
-      <li class="nav-item">
-        <a class="nav-link active" href="cadastro.php">Cadastrar</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="editar.php">Editar</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="exibir.php">Relatório</a>
-      </li>
-      
-    </ul>
-  </div>    
-  </nav>
-
 <div class="container">
 	<div class="d-flex justify-content-center h-100">
 		<div class="card">
 			<div class="card-header">
-				<h3>Cadastrar</h3>
+				<h3>Sign In</h3>
+				<div class="d-flex justify-content-end social_icon">
+					<span><i class="fab fa-facebook-square"></i></span>
+					<span><i class="fab fa-google-plus-square"></i></span>
+					<span><i class="fab fa-twitter-square"></i></span>
+				</div>
 			</div>
 			<div class="card-body">
 				<form>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-user"></i></span>
-						</div>
-						<input type="text" class="form-control" placeholder="username">
-						
-					</div>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-user"></i></span>
-						</div>
-						<input type="text" class="form-control" placeholder="username">
-						
-					</div>
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-user"></i></span>
@@ -66,41 +37,24 @@
 						</div>
 						<input type="password" class="form-control" placeholder="password">
 					</div>
-					
+					<div class="row align-items-center remember">
+						<input type="checkbox">Remember Me
+					</div>
 					<div class="form-group">
-						<input type="submit" value="Login" class="btn btn-info login_btn">
+						<input type="submit" value="Login" class="btn float-right login_btn">
 					</div>
 				</form>
+			</div>
+			<div class="card-footer">
+				<div class="d-flex justify-content-center links">
+					Don't have an account?<a href="#">Sign Up</a>
+				</div>
+				<div class="d-flex justify-content-center">
+					<a href="#">Forgot your password?</a>
+				</div>
 			</div>
 		</div>
 	</div>
 </div>
-<footer>
-              <div class="text-white bg-dark" draggable="true" >
-                <div class="container">
-                  <div class="row">
-                    <div class="p-5 col-md-4">
-                        <h4 class="mb-4 marge">Projeto desenvolvido por</h4>  
-                        <p class="text-center">Nome: Daniel Cunha de Queiroz</p>            
-                     </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-md-12 mt-3">
-                      <p class="text-center">© Copyright 2020 Projeto - Todos os Direitos Reservados. </p>
-                    </div>
-                   </div>
-                  </div>
-              </div>
-</footer>      
-
-<script type="text/javascript">
-       document.getElementById('teste')[0].selected = "selected"; 
-</script>
-            
-  
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="js/bootstrap.min.js"></script>
-
 </body>
 </html>
